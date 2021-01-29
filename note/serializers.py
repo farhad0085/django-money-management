2ecc4fd5c6ce288d2aac9a134aed5ee83ee478d4
Farhad Hossain <farhadhossain0085@gmail.com>
2021-01-29 16:46:57 +0600

    note and tags app and apis are done

--- note/serializers.py
+++ note/serializers.py
@@ -1,11 +1,24 @@
 from rest_framework import serializers
 from .models import Note
-from tag.serializers import TagSerializer
+
 
 class NoteSerializer(serializers.ModelSerializer):
     """Serializer for notes"""
 
-    tags = TagSerializer(many=True)
     class Meta:
         model = Note
         fields = '__all__'
+
+    def create(self, validated_data):
+
+        note = Note.objects.create(
+            user=self.context['request'].user,
+            title=validated_data['title'],
+            body=validated_data['body']
+        )
+
+        for tag in validated_data['tags']:
+            note.tags.add(tag)
+        return note
+
+    
